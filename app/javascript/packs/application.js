b8b8ce0725dd9db3d5b3c3cffc87901b4f10ebd7
Fabio Bernhard <bernhardfabio@gmail.com>
2019-05-31 14:08:50 +0200

    submissions wavesurfer working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,3 +30,26 @@ tracks.forEach((track) => {
   });
 })
 
+// const sub_tracks = document.querySelectorAll(".waveform-sub");
+
+// sub_tracks.forEach((track) => {
+//   let wave = WaveSurfer.create({
+//     container: `#${track.id}`,
+//     waveColor: 'white',
+//     progressColor: '#D1D1D1',
+//     mediaControls: true,
+//     backend: 'MediaElement'
+//     // mediaType: 'audio',
+//     // partialRender: true
+
+//   });
+//   wave.load(track.dataset.trackUrl);
+//   // document.querySelectorAll(track)
+//   wave.getCurrentTime({
+//     container: `#${track.id}`
+//   });
+
+//   wave.on('pause', function () {
+//       wave.params.container.style.opacity = 0.9;
+//   });
+// })

bf17722040fde789f648f455b0cb2155f574564b
Peter Gillis <petergillis@hotmail.com>
2019-06-04 15:15:26 +0200

    routes allow logged-out user to see requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :wikihow]
+  skip_before_action :authenticate_user!, only: [:home ]
 
   def home
   end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index ]
+
   def index
     @requests = Request.all
     if params[:query].present?

6c207be519c01488ae36797b81510a5ff87357ad
Timothy Angus <timangus321@gmail.com>
2019-06-06 13:15:38 +0200

    button dissapears when requester accepts a submission

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_075249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "file"
+    t.string "genre"
     t.text "soundcloud_url"
     t.bigint "genre_id"
     t.index ["genre_id"], name: "index_tracks_on_genre_id"

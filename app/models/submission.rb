aa332be96fc7e3baa5e8c7875fd9f2b02c981a74
Timothy Angus <timangus321@gmail.com>
2019-06-24 22:28:38 +0200

    text, style and some validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,8 @@ class Submission < ApplicationRecord
   mount_uploader :file, FileUploader
   belongs_to :user
   belongs_to :request
+
+  validates :description, presence: true, length: { in: 10..200,
+            message: " must be between 10 - 200 characters." }
+  validates :file, presence: true
 end

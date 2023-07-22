6d02ddfb68cfb87c81a2f7e8be631fd0e9d581e3
Timothy Angus <timangus321@gmail.com>
2023-07-22 16:47:24 +0200

    prevent asset updates

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# Try deploy without these as without yarn this can't run
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rails db:seed

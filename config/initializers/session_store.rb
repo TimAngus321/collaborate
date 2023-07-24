a635904be17d5e4236087aa40d489274e2b84387
Timothy Angus <timangus321@gmail.com>
2023-07-24 20:03:38 +0200

    fix session_storate file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Configurations
-session_url = "#{ENV.fetch('REDIS_CACHE_URL', 'redis://127.0.0.1:6379')}/0/session"
+session_url = "#{ENV.fetch('COLLAB_REDIS_URL', 'redis://127.0.0.1:6379')}/0/session"
 secure = Rails.env.production?
 key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
 domain = ENV.fetch("DOMAIN_NAME", "localhost")
